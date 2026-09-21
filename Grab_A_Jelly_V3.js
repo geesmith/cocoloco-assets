9dca400ee7a0d7872cb62c2de5b2901eea28b67b
geesmith <info@cocolococomps.co.uk>
2026-09-21 10:10:55 +0100

    Adjust image translation in Grab_A_Jelly_V3.js

--- Grab_A_Jelly_V3.js
+++ Grab_A_Jelly_V3.js
@@ -133,7 +133,7 @@ pointer-events:none;
   width:142%;
   height:75%;
   object-fit:fill;
-  transform:translate(5%, 18%);
+  transform:translate(10%, 18%);
   z-index:5;
   pointer-events:none;
   filter:drop-shadow(0 7px 5px #0007);
